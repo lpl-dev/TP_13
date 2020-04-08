2c578de561ceb1ecdd937177219be34e0f65df71
lpl-dev <leo.planquette@isen-ouest.yncrea.fr>
2020-04-08 19:02:34 +0200

    BinaryTree

--- BinaryTree.py
+++ BinaryTree.py
@@ -49,12 +49,75 @@ class BinaryTree:
     def isRoot(self, node):
         return self.__root == node
 
+    def size(self,node):
+        if node==None:
+            return 0
+        return 1+self.size(node.getLeft())+self.size(node.getRight())
+
     def printValues(self,node):
         if node==None:
             return
         self.printValues(node.getLeft())
         print(node.getVal())
         self.printValues(node.getRight())
+
+    def sumValues(self,node):
+        if node==None:
+            return 0
+        return node.getVal()+self.sumValues(node.getLeft())+self.sumValues(node.getRight())
+
+    def numberLeaves(self, node):
+        if node==None:
+            return 0
+        elif node.getLeft()==None and node.getRight()==None:
+            return 1
+        return self.numberLeaves(node.getLeft())+self.numberLeaves(node.getRight())
+
+    def numberInternalNodes(self,node):
+        return self.size(node)-self.numberLeaves(node)
+
+    def height(self, node):
+        if node == None:
+            return 0
+        return max(1 + self.size(node.getLeft()), 1 + self.size(node.getRight()))-1
+
+    def belongs(self,node,val):
+        if node==None:
+            return False
+        if node.getVal()==val:
+            return True
+        return self.belongs(node.getLeft(),val) or self.belongs(node.getRight(),val)
+
+    def ancestors(self,node,val):
+        if self.belongs(node, val):
+            self.__ancestors(node,val)
+
+    def __ancestors(self,node,val):
+        if node==None:
+            return False
+
+        if node.getVal()==val:
+            return True
+
+        if self.__ancestors(node.getLeft(),val) or self.__ancestors(node.getRight(),val):
+            print(node.getVal())
+            return True
+
+    def descendants(self,node,val):
+        if self.belongs(node,val):
+            self.__descendants(node,val)
+
+    def __descendants(self,node,val):
+        if node==None:
+            return
+        if node.getVal()==val:
+            self.printValues(node.getLeft())
+            self.printValues(node.getRight())
+            return
+
+        self.descendants(node.getLeft(),val)
+        self.descendants(node.getRight(),val)
+
 
 if __name__=="__main__":
     values=[12,5,4,3,6,17,19,18,21]
@@ -67,4 +130,23 @@ if __name__=="__main__":
     for node in nodes:
         binaryTree.insert(node)
 
+
+    print("Affichage de l'arbre :")
     binaryTree.printValues(nodes[0])
+    print('-' * 10)
+    print(f"Taille de l'arbre : {binaryTree.size(nodes[0])}")
+    print(f"Somme des valeurs de l'arbre : {binaryTree.sumValues(nodes[0])}")
+    print(f"Hauteur de l'arbre : {binaryTree.height(nodes[0])}")
+    print(f"Nombre de feuilles : {binaryTree.numberLeaves(nodes[0])}")
+    print(f"Nombre de noeuds internes : {binaryTree.numberInternalNodes(nodes[0])}")
+    print('-'*10)
+    print(f"12 une valeur de l'arbre ? {binaryTree.belongs(nodes[0],12)}")
+    print(f"4 une valeur de l'arbre ? {binaryTree.belongs(nodes[0],4)}")
+    print(f"17 une valeur de l'arbre ? {binaryTree.belongs(nodes[0],17)}")
+    print(f"23 une valeur de l'arbre ? {binaryTree.belongs(nodes[0],23)}")
+    print('-'*10)
+    print("Affichage des antécédents de 3 :")
+    binaryTree.ancestors(nodes[0],21)
+    print('-' * 10)
+    print("Affichage des descendants de 5 :")
+    binaryTree.descendants(nodes[0],5)
